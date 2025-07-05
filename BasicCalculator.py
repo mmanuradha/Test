fa22e6714a7d70b4a50a75e412d2b97e7bae1a44
mmanuradha <mmanuradha@gmail.com>
2025-07-06 01:00:57 +0530

    Add files via upload

---
+++ BasicCalculator.py
@@ -0,0 +1,24 @@
+# Simple calculator for two numbers
+
+# Get user input
+num1 = float(input("Enter the first number: "))
+operator = input("Enter an operator (+, -, *, /): ")
+num2 = float(input("Enter the second number: "))
+
+# Perform calculation
+if operator == '+':
+    result = num1 + num2
+elif operator == '-':
+    result = num1 - num2
+elif operator == '*':
+    result = num1 * num2
+elif operator == '/':
+    if num2 != 0:
+        result = num1 / num2
+    else:
+        result = "Error! Division by zero."
+else:
+    result = "Invalid operator!"
+
+# Show result
+print("Result:", result)
